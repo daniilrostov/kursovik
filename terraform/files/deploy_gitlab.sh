ad5525766b557691b58bb2971fb83248e1bb13ad
Наиль Камалеев <nkamaleev@astralinux.ru>
2022-08-12 23:58:18 +0300

    Add kubectl and kubeconfig

--- terraform/files/deploy_gitlab.sh
+++ terraform/files/deploy_gitlab.sh
@@ -15,7 +15,10 @@ echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose docker-compose-plugin -y
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose docker-compose-plugin curl -y
 sudo mkdir -p /srv/gitlab/config /srv/gitlab/data /srv/gitlab/logs 
 cd /tmp
 sudo docker-compose up -d
+curl -LO https://storage.googleapis.com/kubernetes-release/release/v1.22.0/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+sudo mv ./kubectl /usr/bin/kubectl
