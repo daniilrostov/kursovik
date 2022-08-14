2f01d8c93f1d5ef4f23620b3382442c7b139005d
Наиль Камалеев <nkamaleev@astralinux.ru>
2022-08-14 23:34:08 +0300

    Add ingress and prometheus with grafana

--- terraform/files/deploy_gitlab.sh
+++ terraform/files/deploy_gitlab.sh
@@ -15,7 +15,7 @@ echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose docker-compose-plugin curl -y
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose docker-compose-plugin curl gitlab-runner -y
 sudo mkdir -p /srv/gitlab/config /srv/gitlab/data /srv/gitlab/logs 
 cd /tmp
 sudo docker-compose up -d
--- terraform/main.tf
+++ terraform/main.tf
@@ -15,13 +15,13 @@ provider "yandex" {
 
 resource "yandex_compute_instance" "gitlab" {
   name = "gitlab"
-
+  platform_id = "standard-v2"
   labels = {
     tags = "gitlab"
   }
   resources {
-    cores  = 2
-    memory = 4
+    cores  = 4
+    memory = 8
   }
 
   boot_disk {
@@ -74,45 +74,101 @@ resource "yandex_kubernetes_cluster" "kubernetes" {
      subnet_id = var.subnet_id
    }
    public_ip = true
+
+   security_group_ids = [
+      yandex_vpc_security_group.k8s-main-sg.id,
+      yandex_vpc_security_group.k8s-master-whitelist.id
+   ]
  }
  service_account_id      = var.sa_id
  node_service_account_id = var.sa_id
-#    depends_on = [
-#      yandex_resourcemanager_folder_iam_binding.editor,
-#      yandex_resourcemanager_folder_iam_binding.images-puller
-#    ]
 }
 
-# resource "yandex_vpc_network" "<имя сети>" { name = "<имя сети>" }
+resource "yandex_vpc_security_group" "k8s-main-sg" {
+  name        = "k8s-main-sg"
+  description = "Правила группы обеспечивают базовую работоспособность кластера. Примените ее к кластеру и группам узлов."
+  network_id  = var.network_id
+  ingress {
+    protocol       = "TCP"
+    description    = "Правило разрешает проверки доступности с диапазона адресов балансировщика нагрузки. Нужно для работы отказоустойчивого кластера и сервисов балансировщика."
+    v4_cidr_blocks = ["198.18.235.0/24", "198.18.248.0/24"]
+    from_port      = 0
+    to_port        = 65535
+  }
+  ingress {
+    protocol          = "ANY"
+    description       = "Правило разрешает взаимодействие мастер-узел и узел-узел внутри группы безопасности."
+    predefined_target = "self_security_group"
+    from_port         = 0
+    to_port           = 65535
+  }
+  ingress {
+    protocol       = "ANY"
+    description    = "Правило разрешает взаимодействие под-под и сервис-сервис. Укажите подсети вашего кластера и сервисов."
+    v4_cidr_blocks = ["10.128.0.0/24"]
+    from_port      = 0
+    to_port        = 65535
+  }
+  ingress {
+    protocol       = "ICMP"
+    description    = "Правило разрешает отладочные ICMP-пакеты из внутренних подсетей."
+    v4_cidr_blocks = ["172.16.0.0/12"]
+  }
+  egress {
+    protocol       = "ANY"
+    description    = "Правило разрешает весь исходящий трафик. Узлы могут связаться с Yandex Container Registry, Object Storage, Docker Hub и т. д."
+    v4_cidr_blocks = ["0.0.0.0/0"]
+    from_port      = 0
+    to_port        = 65535
+  }
+}
 
-# resource "yandex_vpc_subnet" "<имя подсети>" {
-#  v4_cidr_blocks = ["<диапазон адресов подсети>"]
-#  zone           = "<зона доступности>"
-#  network_id     = yandex_vpc_network.<имя сети>.id
-# }
+resource "yandex_vpc_security_group" "k8s-public-services" {
+  name        = "k8s-public-services"
+  description = "Правила группы разрешают подключение к сервисам из интернета. Примените правила только для групп узлов."
+  network_id  = var.network_id
 
-# resource "yandex_iam_service_account" "<имя сервисного аккаунта>" {
-#  name        = "<имя сервисного аккаунта>"
-#  description = "<описание сервисного аккаунта>"
-# }
+  ingress {
+    protocol       = "TCP"
+    description    = "Правило разрешает входящий трафик из интернета на диапазон портов NodePort. Добавьте или измените порты на нужные вам."
+    v4_cidr_blocks = ["0.0.0.0/0"]
+    from_port      = 30000
+    to_port        = 32767
+  }
+}
 
-# resource "yandex_resourcemanager_folder_iam_binding" "editor" {
-#  # Сервисному аккаунту назначается роль "editor".
-#  folder_id = "<идентификатор каталога>"
-#  role      = "editor"
-#  members   = [
-#    "serviceAccount:${yandex_iam_service_account.<имя сервисного аккаунта>.id}"
-#  ]
-# }
+resource "yandex_vpc_security_group" "k8s-nodes-ssh-access" {
+  name        = "k8s-nodes-ssh-access"
+  description = "Правила группы разрешают подключение к узлам кластера по SSH. Примените правила только для групп узлов."
+  network_id  = var.network_id
 
-# resource "yandex_resourcemanager_folder_iam_binding" "images-puller" {
-#  # Сервисному аккаунту назначается роль "container-registry.images.puller".
-#  folder_id = "<идентификатор каталога>"
-#  role      = "container-registry.images.puller"
-#  members   = [
-#    "serviceAccount:${yandex_iam_service_account.<имя сервисного аккаунта>.id}"
-#  ]
-# }
+  ingress {
+    protocol       = "TCP"
+    description    = "Правило разрешает подключение к узлам по SSH с указанных IP-адресов."
+    v4_cidr_blocks = ["0.0.0.0/0"]
+    port           = 22
+  }
+}
+
+resource "yandex_vpc_security_group" "k8s-master-whitelist" {
+  name        = "k8s-master-whitelist"
+  description = "Правила группы разрешают доступ к API Kubernetes из интернета. Примените правила только к кластеру."
+  network_id  = var.network_id
+
+  ingress {
+    protocol       = "TCP"
+    description    = "Правило разрешает подключение к API Kubernetes через порт 6443 из указанной сети."
+    v4_cidr_blocks = ["0.0.0.0/0"]
+    port           = 6443
+  }
+
+  ingress {
+    protocol       = "TCP"
+    description    = "Правило разрешает подключение к API Kubernetes через порт 443 из указанной сети."
+    v4_cidr_blocks = ["0.0.0.0/0"]
+    port           = 443
+  }
+}
 
 resource "yandex_kubernetes_node_group" "nodes" {
   cluster_id = yandex_kubernetes_cluster.kubernetes.id
@@ -135,19 +191,15 @@ resource "yandex_kubernetes_node_group" "nodes" {
     }
 
     network_interface {
-      # Указан id подсети default-ru-central1-a
       subnet_ids = ["${var.subnet_id}"]
       nat        = true
-    #   security_group_ids = [
-    #     yandex_vpc_security_group.k8s-main-sg.id,
-    #     yandex_vpc_security_group.k8s-nodes-ssh-access.id,
-    #     yandex_vpc_security_group.k8s-public-services.id
-    #   ]
+      security_group_ids = [
+        yandex_vpc_security_group.k8s-main-sg.id,
+        yandex_vpc_security_group.k8s-nodes-ssh-access.id,
+        yandex_vpc_security_group.k8s-public-services.id
+      ]
     }
 
-    # metadata = {
-    #   user-data = "${file("./metadata")}"
-    # }
   }
 
   scale_policy {
