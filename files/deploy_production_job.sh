f0c39ac903ba79edf51f2926b4f25eaceb5158ec
Наиль Камалеев <nkamaleev@astralinux.ru>
2022-08-22 23:23:56 +0300

    Add environment

--- files/deploy_production_job.sh
+++ files/deploy_production_job.sh
@@ -6,17 +6,18 @@ docker push daniilrostov/ui:1.0
 docker push daniilrostov/crawler:1.0
 docker push daniilrostov/prometheus:1.0
 docker push daniilrostov/grafana:1.0
-kubectl apply -f ./kubernetes/mongo-deployment.yml --kubeconfig=/opt/kubeconfig
-kubectl apply -f ./kubernetes/mongo-service.yml --kubeconfig=/opt/kubeconfig
-kubectl apply -f ./kubernetes/rabbitmq-deployment.yml --kubeconfig=/opt/kubeconfig
-kubectl apply -f ./kubernetes/rabbitmq-service.yml --kubeconfig=/opt/kubeconfig
-kubectl apply -f ./kubernetes/crawler-deployment.yml --kubeconfig=/opt/kubeconfig
-kubectl apply -f ./kubernetes/crawler-service.yml --kubeconfig=/opt/kubeconfig
-kubectl apply -f ./kubernetes/ui-deployment.yml --kubeconfig=/opt/kubeconfig
-kubectl apply -f ./kubernetes/ui-service.yml --kubeconfig=/opt/kubeconfig
-kubectl apply -f ./kubernetes/ingress.yml --kubeconfig=/opt/kubeconfig
-kubectl apply -f ./kubernetes/ingress-prometheus.yml --kubeconfig=/opt/kubeconfig
-kubectl apply -f ./kubernetes/prometheus-deployment.yml --kubeconfig=/opt/kubeconfig
-kubectl apply -f ./kubernetes/prometheus-service.yml --kubeconfig=/opt/kubeconfig
-kubectl apply -f ./kubernetes/grafana-deployment.yml --kubeconfig=/opt/kubeconfig
-kubectl apply -f ./kubernetes/grafana-service.yml --kubeconfig=/opt/kubeconfig
+kubectl create namespace prod
+kubectl apply -f ./kubernetes/mongo-deployment.yml --kubeconfig=/opt/kubeconfig -n prod
+kubectl apply -f ./kubernetes/mongo-service.yml --kubeconfig=/opt/kubeconfig -n prod
+kubectl apply -f ./kubernetes/rabbitmq-deployment.yml --kubeconfig=/opt/kubeconfig -n prod
+kubectl apply -f ./kubernetes/rabbitmq-service.yml --kubeconfig=/opt/kubeconfig -n prod
+kubectl apply -f ./kubernetes/crawler-deployment.yml --kubeconfig=/opt/kubeconfig -n prod
+kubectl apply -f ./kubernetes/crawler-service.yml --kubeconfig=/opt/kubeconfig -n prod
+kubectl apply -f ./kubernetes/ui-deployment.yml --kubeconfig=/opt/kubeconfig -n prod
+kubectl apply -f ./kubernetes/ui-service.yml --kubeconfig=/opt/kubeconfig -n prod
+kubectl apply -f ./kubernetes/ingress-prod.yml --kubeconfig=/opt/kubeconfig -n prod
+kubectl apply -f ./kubernetes/ingress-prometheus-prod.yml --kubeconfig=/opt/kubeconfig -n prod
+kubectl apply -f ./kubernetes/prometheus-deployment.yml --kubeconfig=/opt/kubeconfig -n prod
+kubectl apply -f ./kubernetes/prometheus-service.yml --kubeconfig=/opt/kubeconfig -n prod
+kubectl apply -f ./kubernetes/grafana-deployment.yml --kubeconfig=/opt/kubeconfig -n prod
+kubectl apply -f ./kubernetes/grafana-service.yml --kubeconfig=/opt/kubeconfig -n prod
