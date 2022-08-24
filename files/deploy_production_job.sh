1c377005f31196c73fa088a1c01a8dece8daa408
Даниил Ростов <drostov@astralinux.ru>
2022-08-25 00:48:53 +0300

    Change vars monifest

--- files/deploy_production_job.sh
+++ files/deploy_production_job.sh
@@ -13,27 +13,27 @@ kubectl delete -f ./kubernetes/mongo-deployment.yml -n prod --kubeconfig=/opt/ku
 kubectl delete -f ./kubernetes/mongo-service.yml -n prod --kubeconfig=/opt/kubeconfig
 kubectl delete -f ./kubernetes/rabbitmq-deployment.yml -n prod --kubeconfig=/opt/kubeconfig
 kubectl delete -f ./kubernetes/rabbitmq-service.yml -n prod --kubeconfig=/opt/kubeconfig
-kubectl delete -f ./kubernetes/crawler-deployment.yml -n prod --kubeconfig=/opt/kubeconfig
+IMAGE_TAG=1.0 envsubst < crawler-deployment.yml | kubectl delete -f - -n prod --kubeconfig=/opt/kubeconfig
 kubectl delete -f ./kubernetes/crawler-service.yml -n prod --kubeconfig=/opt/kubeconfig
-kubectl delete -f ./kubernetes/ui-deployment.yml -n prod --kubeconfig=/opt/kubeconfig
+IMAGE_TAG=1.0 envsubst < ui-deployment.yml | kubectl delete -f - -n prod --kubeconfig=/opt/kubeconfig
 kubectl delete -f ./kubernetes/ui-service.yml -n prod --kubeconfig=/opt/kubeconfig
 kubectl delete -f ./kubernetes/ingress-prod.yml -n prod --kubeconfig=/opt/kubeconfig
 kubectl delete -f ./kubernetes/ingress-prometheus-prod.yml -n prod --kubeconfig=/opt/kubeconfig
 kubectl delete -f ./kubernetes/prometheus-deployment.yml -n prod --kubeconfig=/opt/kubeconfig
 kubectl delete -f ./kubernetes/prometheus-service.yml -n prod --kubeconfig=/opt/kubeconfig
-kubectl delete -f ./kubernetes/grafana-deployment.yml -n prod --kubeconfig=/opt/kubeconfig
+IMAGE_TAG=1.0 envsubst < grafana-deployment.yml | kubectl delete -f - -n prod --kubeconfig=/opt/kubeconfig
 kubectl delete -f ./kubernetes/grafana-service.yml -n prod --kubeconfig=/opt/kubeconfig
 kubectl apply -f ./kubernetes/mongo-deployment.yml -n prod --kubeconfig=/opt/kubeconfig
 kubectl apply -f ./kubernetes/mongo-service.yml -n prod --kubeconfig=/opt/kubeconfig
 kubectl apply -f ./kubernetes/rabbitmq-deployment.yml -n prod --kubeconfig=/opt/kubeconfig
 kubectl apply -f ./kubernetes/rabbitmq-service.yml -n prod --kubeconfig=/opt/kubeconfig
-kubectl apply -f ./kubernetes/crawler-deployment.yml -n prod --kubeconfig=/opt/kubeconfig
+IMAGE_TAG=1.0 envsubst < crawler-deployment.yml | kubectl apply -f - -n prod --kubeconfig=/opt/kubeconfig
 kubectl apply -f ./kubernetes/crawler-service.yml -n prod --kubeconfig=/opt/kubeconfig
-kubectl apply -f ./kubernetes/ui-deployment.yml -n prod --kubeconfig=/opt/kubeconfig
+IMAGE_TAG=1.0 envsubst < ui-deployment.yml | kubectl apply -f - -n prod --kubeconfig=/opt/kubeconfig
 kubectl apply -f ./kubernetes/ui-service.yml -n prod --kubeconfig=/opt/kubeconfig
 kubectl apply -f ./kubernetes/ingress-prod.yml -n prod --kubeconfig=/opt/kubeconfig
 kubectl apply -f ./kubernetes/ingress-prometheus-prod.yml -n prod --kubeconfig=/opt/kubeconfig
 kubectl apply -f ./kubernetes/prometheus-deployment.yml -n prod --kubeconfig=/opt/kubeconfig
 kubectl apply -f ./kubernetes/prometheus-service.yml -n prod --kubeconfig=/opt/kubeconfig
-kubectl apply -f ./kubernetes/grafana-deployment.yml -n prod --kubeconfig=/opt/kubeconfig
+IMAGE_TAG=1.0 envsubst < grafana-deployment.yml | kubectl apply -f - -n prod --kubeconfig=/opt/kubeconfig
 kubectl apply -f ./kubernetes/grafana-service.yml -n prod --kubeconfig=/opt/kubeconfig
