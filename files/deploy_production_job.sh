0cbd6c5542e310e06bcdfd8d59d6b09e5f197bdc
Даниил Ростов <drostov@astralinux.ru>
2022-08-24 23:58:22 +0300

    Change monigests

--- files/deploy_production_job.sh
+++ files/deploy_production_job.sh
@@ -9,6 +9,20 @@ docker push daniilrostov/prometheus:1.0
 docker push daniilrostov/grafana:1.0
 docker-compose down --rm local
 kubectl create namespace prod --kubeconfig=/opt/kubeconfig
+kubectl delete -f ./kubernetes/mongo-deployment.yml -n prod --kubeconfig=/opt/kubeconfig
+kubectl delete -f ./kubernetes/mongo-service.yml -n prod --kubeconfig=/opt/kubeconfig
+kubectl delete -f ./kubernetes/rabbitmq-deployment.yml -n prod --kubeconfig=/opt/kubeconfig
+kubectl delete -f ./kubernetes/rabbitmq-service.yml -n prod --kubeconfig=/opt/kubeconfig
+kubectl delete -f ./kubernetes/crawler-deployment.yml -n prod --kubeconfig=/opt/kubeconfig
+kubectl delete -f ./kubernetes/crawler-service.yml -n prod --kubeconfig=/opt/kubeconfig
+kubectl delete -f ./kubernetes/ui-deployment.yml -n prod --kubeconfig=/opt/kubeconfig
+kubectl delete -f ./kubernetes/ui-service.yml -n prod --kubeconfig=/opt/kubeconfig
+kubectl delete -f ./kubernetes/ingress-prod.yml -n prod --kubeconfig=/opt/kubeconfig
+kubectl delete -f ./kubernetes/ingress-prometheus-prod.yml -n prod --kubeconfig=/opt/kubeconfig
+kubectl delete -f ./kubernetes/prometheus-deployment.yml -n prod --kubeconfig=/opt/kubeconfig
+kubectl delete -f ./kubernetes/prometheus-service.yml -n prod --kubeconfig=/opt/kubeconfig
+kubectl delete -f ./kubernetes/grafana-deployment.yml -n prod --kubeconfig=/opt/kubeconfig
+kubectl delete -f ./kubernetes/grafana-service.yml -n prod --kubeconfig=/opt/kubeconfig
 kubectl apply -f ./kubernetes/mongo-deployment.yml -n prod --kubeconfig=/opt/kubeconfig
 kubectl apply -f ./kubernetes/mongo-service.yml -n prod --kubeconfig=/opt/kubeconfig
 kubectl apply -f ./kubernetes/rabbitmq-deployment.yml -n prod --kubeconfig=/opt/kubeconfig
